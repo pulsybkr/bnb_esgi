fe68899403ddeab67ecb0cb248b7dedf66248aa1
Pulsy BACKEKOLO <pulsycaleb@gmail.com>
2025-09-23 11:10:54 +0200

    correction du docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,4 +16,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
 })
